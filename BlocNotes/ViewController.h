e7fd47262cfbd5367d83512b385c4f181ec1dd6c
Stephen Murphy <smurphylaw@gmail.com>
2015-05-30 16:53:07 -0600

    Initial Commit

---
+++ BlocNotes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocNotes
+//
+//  Created by Murphy on 5/30/15.
+//  Copyright (c) 2015 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlocNotes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlocNotes
+//
+//  Created by Murphy on 5/30/15.
+//  Copyright (c) 2015 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
